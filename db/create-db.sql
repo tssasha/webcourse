51d341a17e543ae57c9a52ffe17776823593f178
Sasha Tsys <s.tsys@ispras.ru>
2021-02-18 22:47:40 +0300

    create and fill db

---
+++ db/create-db.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE forum;
+
+CREATE TABLE IF NOT EXISTS users (
+    user_login text PRIMARY KEY,
+    user_password text CHECK (char_length(user_password) >= 8),
+    reg_date date DEFAULT CURRENT_DATE,
+    rights text DEFAULT 'Пользователь',
+    description text
+);
+
+CREATE TABLE IF NOT EXISTS sections (
+    topic_name text PRIMARY KEY,
+    section_name text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS files (
+    file text PRIMARY KEY,  --path to file
+    message_no integer REFERENCES topics
+);
+
+CREATE TABLE IF NOT EXISTS topics (
+    message_no integer PRIMARY KEY,
+    topic_name text REFERENCES sections,
+    message text NOT NULL,
+    user_login text REFERENCES users,
+    time_stamp date DEFAULT CURRENT_DATE
+);
